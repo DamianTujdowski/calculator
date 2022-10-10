1b73b2366554af304e1c8fd10a4e3c04913d4b46
damian.tujdowski <damian.tujdowski@wundermanthompson.com>
2022-10-10 22:14:33 +0200

    Add test

---
+++ src/test/java/pl/licz/calculator/CalculationsControllerTest.java
@@ -0,0 +1,36 @@
+package pl.licz.calculator;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(SpringExtension.class)
+class CalculationsControllerTest {
+
+    @TestConfiguration
+    static class CalculationsControllerTestImpl {
+        @Bean
+        public CalculationsController calculationsController() {
+            return new CalculationsController();
+        }
+    }
+
+    @Autowired
+    private CalculationsController cc;
+    int a;
+    int b;
+
+    @Test
+    void addShouldReturn8() {
+        a = 3;
+        b = 5;
+        int res = cc.add(a, b);
+        assertEquals(8, res);
+    }
+
+}
